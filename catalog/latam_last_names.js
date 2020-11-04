3d940b774e1affbc3441d11edee30d7e67973dd4
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2020-11-04 17:13:19 -0600

    Initial commit

---
+++ catalog/latam_last_names.js
@@ -0,0 +1,14 @@
+module.exports = [
+    "Toledo",
+    "Santiago",
+    "Quecha",
+    "Montero",
+    "Lopez",
+    "Jimenez",
+    "Sanchez",
+    "Ruiz",
+    "Valivieso",
+    "Guerra",
+    "Aquino",
+    "Figueroa",
+];
